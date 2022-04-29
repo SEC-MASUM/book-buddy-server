a279c43e1844b9d149d295a6bb21c24056ed0580
SEC-MASUM <masum.jess42@gmail.com>
2022-04-30 00:48:31 +0600

    fix : fix PORT spelling

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const host = "localhost";
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 // Database connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vt98y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,11 +18,6 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
 });
 
 async function run() {
